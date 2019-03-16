6b1e8b991992f0bf0588c55482d66e89a2a28bfd
Metal21 <biametal21@gmail.com>
2019-03-16 17:35:26 +0300

    Second lesson. Spring Boot JPA(Hibernate)

---
+++ src/main/java/com/study/sweater/repos/MessageRepo.java
@@ -0,0 +1,12 @@
+package com.study.sweater.repos;
+
+import com.study.sweater.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Integer> {
+
+    List<Message> findByTag(String tag);
+
+}
