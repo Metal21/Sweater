468a828e651088c9592ffd287a26866eccaa5dd1
Metal21 <biametal21@gmail.com>
2019-03-16 21:15:15 +0300

    Fourth lesson. Авторизация с помощью UserService. Передача в контроллер информации о пользователе, который делает запрос.

--- src/main/java/com/study/sweater/controler/MainController.java
+++ src/main/java/com/study/sweater/controler/MainController.java
@@ -1,8 +1,10 @@
 package com.study.sweater.controler;
 
 import com.study.sweater.domain.Message;
+import com.study.sweater.domain.User;
 import com.study.sweater.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,10 +32,12 @@ public class MainController {
     }
 
     @PostMapping("addMessage")
-    public String add(@RequestParam String text,
-                      @RequestParam String tag,
-                      Map<String,Object> model){
-        Message message = new Message(text,tag);
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag,
+            Map<String,Object> model){
+        Message message = new Message(text,tag,user);
         messageRepo.save(message);
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages",messages);
--- src/main/java/com/study/sweater/domain/User.java
+++ src/main/java/com/study/sweater/domain/User.java
@@ -1,14 +1,17 @@
 package com.study.sweater.domain;
 
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.Set;
 
 @Entity
 @Table(name = "usr")
 @Data
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -22,4 +25,24 @@ public class User {
     @CollectionTable(name = "usr_role",joinColumns  = @JoinColumn(name = "user_id"))
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    public boolean isEnabled() {
+        return isActive();
+    }
 }
